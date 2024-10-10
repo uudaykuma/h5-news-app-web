3b505247786f8c1df831f67eeba367039760ef38
Digvijay singh <digvijay.mynt@perennialsys.com>
2024-10-10 13:00:54 +0530

    add the new folder

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Stage 1: Build the application
-FROM node:16-alpine AS build
+FROM node:18-alpine AS build
 
 WORKDIR /app
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Hello this is news application</h1>
+  </main>
+</template>
